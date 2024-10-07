7fb01b56c79adf4f15d73109e53b3bb9ce06854e
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-10-07 20:03:23 +0700

    update: change một tí logic ở hàm main để test.

--- datawarehouse-crawl/src/main/java/com/nlu/app/DatawarehouseCrawlApplication.java
+++ datawarehouse-crawl/src/main/java/com/nlu/app/DatawarehouseCrawlApplication.java
@@ -1,5 +1,6 @@
 package com.nlu.app;
 
+import com.nlu.app.service.DataCrawlService;
 import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -7,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.SpringApplication;
@@ -18,7 +20,7 @@ import java.util.List;
 @SpringBootApplication
 @RequiredArgsConstructor
 public class DatawarehouseCrawlApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(DatawarehouseCrawlApplication.class, args);
 
         // Tạo ChromeOptions và set chế độ chạy ẩn
@@ -26,11 +28,11 @@ public class DatawarehouseCrawlApplication {
 
         // Khởi tạo ChromeDriver với ChromeOptions
         WebDriver driver = new ChromeDriver(options);
+        Actions actions = new Actions(driver);
         driver.get("https://cellphones.com.vn/phu-kien/chuot-ban-phim-may-tinh/chuot.html");
 
         // Khởi tạo WebDriverWait
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
         while (true) {
             try {
                 // Kiểm tra xem nút "Show More" có tồn tại không
@@ -44,7 +46,7 @@ public class DatawarehouseCrawlApplication {
 
                 // Kiểm tra xem nút có thể bấm được hay không
                 if (!showMoreButton.isEnabled() || !showMoreButton.isDisplayed()) {
-                    System.out.println("Nút Show More không còn khả năng bấm.");
+//                    System.out.println("Nút Show More không còn khả năng bấm.");
                     break; // Thoát vòng lặp khi nút không còn khả năng bấm
                 }
 
@@ -53,32 +55,39 @@ public class DatawarehouseCrawlApplication {
                 wait.until(ExpectedConditions.not(ExpectedConditions.attributeContains(showMoreButton, "class", "is-large")));
 
                 // Bấm vào nút để load thêm dữ liệu
-                showMoreButton.click();
+                actions.moveToElement(showMoreButton).click().perform();
 
                 Thread.sleep(1000); // Tạm dừng để đảm bảo dữ liệu mới được tải
 
                 // Chờ dữ liệu mới xuất hiện
                 wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".product-info")));
-
-                System.out.println("Đã tải thêm sản phẩm.");
             } catch (Exception e) {
                 e.printStackTrace();
                 // Thoát vòng lặp nếu có bất kỳ lỗi gì khác
                 break;
             }
         }
-
         // Tìm tất cả các phần tử có class là 'product-info'
-        List<WebElement> elements = driver.findElements(By.cssSelector(".product-info a[href]"));
+        List<WebElement> products = driver.findElements(By.cssSelector(".product-info"));
 
-        // In ra src của các phần tử tìm được
-        for (WebElement product : elements) {
-            String productLink = product.getAttribute("href");
-            System.out.println(productLink);
+        for (WebElement product : products) {
+            try {
+                    var href = product.findElement(By.cssSelector("a[href]"));
+                    var name = product.findElement(By.cssSelector(".product__name")).getText();
+                    var imageLink = product.findElement(By.cssSelector(".product__image img")).getAttribute("src");
+                    String productLink = href.getAttribute("href");
+                    System.out.println("Name: "+name);
+                    System.out.println("img url: "+imageLink);
+                    System.out.println("source: "+ productLink);
+                    DataCrawlService.crawl(productLink, driver);
+                    System.out.println("-------------------------------------------");
+                System.out.println("Tổng số sản phẩm load được: "+products.size());
+            } catch (Exception e) {
+                e.printStackTrace(); // skip element bị lỗi
+            } finally {
+                driver.quit();
+            }
         }
-        System.out.println("Tổng số sản phẩm load được: "+elements.size());
-        // Đóng trình duyệt sau khi hoàn thành công việc
-        driver.quit();
     }
 
 }
