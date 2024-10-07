d9ff9dee3d571f9e0bb7333b846898ac660c6ce9
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-10-07 20:41:08 +0700

    update: thêm logic viết xuống dữ liệu temp xuống file csv.

--- datawarehouse-crawl/src/main/java/com/nlu/app/service/DataCrawlService.java
+++ datawarehouse-crawl/src/main/java/com/nlu/app/service/DataCrawlService.java
@@ -19,36 +19,45 @@ import java.util.List;
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class DataCrawlService {
 
-    public static void crawl(String webURL, WebDriver driver) throws InterruptedException {
-        // Khởi tạo ChromeDriver với ChromeOptions
-        driver.get(webURL);
-        Actions actions = new Actions(driver);
+    public static String crawl(String webURL) throws InterruptedException {
+        var driver = new ChromeDriver();
+        try {
+            // Khởi tạo ChromeDriver với ChromeOptions
+            driver.get(webURL);
+            Actions actions = new Actions(driver);
 
-        // Cuộn trang xuống bằng phím Page Down
-        for (int i = 0; i < 5; i++) { // Cuộn xuống 5 lần
-            actions.sendKeys(Keys.PAGE_DOWN).perform();
-            Thread.sleep(500); // Tạm dừng một chút để trang kịp tải nội dung mới
+            // Cuộn trang xuống bằng phím Page Down
+            for (int i = 0; i < 5; i++) { // Cuộn xuống 5 lần
+                actions.sendKeys(Keys.PAGE_DOWN).perform();
+                Thread.sleep(500); // Tạm dừng một chút để trang kịp tải nội dung mới
+            }
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(25));
+            WebElement buttonBlock = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".cps-block-technicalInfo")));
+            actions.scrollToElement(buttonBlock);
+            var button = driver.findElements(By.cssSelector(".cps-block-technicalInfo .button__show-modal-technical")).get(0);
+            actions.moveToElement(button).click().perform();
+            var modalContent = driver.findElement(By.cssSelector(".technical-content-modal"));
+            // Nếu bạn muốn phân tích thêm các phần tử con trong modal
+//        List<WebElement> childElements = modalContent.findElements(By.cssSelector(".modal-item-description.mx-2>div")); // Tìm tất cả các phần tử con
+//
+//        for (WebElement child : childElements) {
+//            var attribute = child.findElement(By.cssSelector("p")).getText();
+//            var value = child.findElement(By.cssSelector("div")).getText();
+//            System.out.printf("%s = %s", attribute, value);
+//            System.out.println();
+//        }
+            var result = modalContent.getAttribute("outerHTML");
+            return result;
+        } finally {
+            driver.quit();
         }
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(25));
-        WebElement buttonBlock = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".cps-block-technicalInfo")));
-        actions.scrollToElement(buttonBlock);
-        var button = driver.findElements(By.cssSelector(".cps-block-technicalInfo .button__show-modal-technical")).get(0);
-        actions.moveToElement(button).click().perform();
-        var modalContent = driver.findElement(By.cssSelector(".technical-content-modal"));
-        // Nếu bạn muốn phân tích thêm các phần tử con trong modal
-        List<WebElement> childElements = modalContent.findElements(By.cssSelector(".modal-item-description.mx-2>div")); // Tìm tất cả các phần tử con
-
-        for (WebElement child : childElements) {
-            var attribute = child.findElement(By.cssSelector("p")).getText();
-            var value = child.findElement(By.cssSelector("div")).getText();
-            System.out.printf("%s = %s", attribute, value);
-            System.out.println();
-        }
-        driver.quit();
     }
 
     public static void main(String[] args) throws InterruptedException {
-        DataCrawlService.crawl("https://cellphones.com.vn/chuot-gaming-razer-basilisk-v3.html", new ChromeDriver());
+        var driver = new ChromeDriver();
+        String test = DataCrawlService.crawl("https://cellphones.com.vn/chuot-gaming-razer-basilisk-v3.html");
+        System.out.println(test);
+        driver.quit();
     }
 }
