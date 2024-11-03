91786e1d02c7311d5ee610cec3ea6540d2d9fb35
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-11-03 21:26:10 +0700

    Update lại code cho service crawl data từ cell phone

---
+++ datawarehouse-crawl/src/main/java/com/nlu/app/service/crawler/Crawler.java
@@ -0,0 +1,21 @@
+package com.nlu.app.service.crawler;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.List;
+
+public interface Crawler {
+    /**
+     * Logic crawl ra cấu trúc HTML mà tại đó, có thể extract được đầy đủ các field cần thiết của một sản phẩm.
+     * @param source nguồn sản phẩm
+     * @return cấu trúc HTML dưới dạng String của một sản phẩm
+     */
+    String prepareHTML(String source);
+    WebDriver prepareDriver(String source);
+    /**
+     * Logic chuẩn bị trích xuất nguồn dữ liệu từ một trang cụ thể.
+     * @param page Trang chứa các nguồn sản phẩm
+     * @return một mảng link nguồn dữ liệu cho từng sản phẩm.
+     */
+    List<String> prepareSources(String page);
+}
---
+++ datawarehouse-crawl/src/main/java/com/nlu/app/service/extractor/Extractor_Cellphones.java
@@ -0,0 +1,39 @@
+package com.nlu.app.service.extractor;
+import com.nlu.app.pojo.CrawlFields;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Extractor_Cellphones implements Extractor {
+    @Override
+    public String[] extract(String html, List<CrawlFields> fields) {
+        int col = fields.size();
+        var result = new String[fields.size()];
+        Document doc = Jsoup.parse(html);
+        for (int i = 0; i < col; i++) {
+            var field = fields.get(i);
+            var elements = this.extractElements(doc, field);
+            int size = elements.size();
+            String data;
+            if (size == 0) {
+                data = "";
+                System.out.println("field: "+ field.getField_name() +" bị thiếu data");
+                result[i] = data;
+            }
+            else if (size == 1) {
+                data = this.extractSingleData(elements.get(0), field);
+                System.out.println("field: "+ field.getField_name() +", data: "+data);
+                result[i] = data;
+            }
+            else {
+                ArrayList<String> arr = new ArrayList<>();
+                for (var element: elements) arr.add(this.extractSingleData(element, field));
+                data = String.join(", ", arr);
+                System.out.println("field: "+ field.getField_name() +", data: "+data);
+                result[i] = data;
+            }
+        }
+        return result;
+    }
+}
