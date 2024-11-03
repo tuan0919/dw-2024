85b3f6707a65702e5b8bfa177bc649313bf1f811
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-11-04 04:59:16 +0700

    thêm abstract class Service

---
+++ datawarehouse-crawl/src/main/java/com/nlu/app/service/process/Service.java
@@ -0,0 +1,34 @@
+package com.nlu.app.service.process;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nlu.app.pojo.CrawlProperties;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public abstract class Service {
+    final CrawlProperties crawlProperties;
+    final FileService fileService;
+    List<String[]> memory;
+
+    public Service(String propertiesJSON) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        this.crawlProperties = objectMapper.readValue(propertiesJSON, CrawlProperties.class);
+        this.fileService = new FileService();
+        this.memory = new ArrayList<>();
+    }
+
+    public abstract String[] crawlProduct(String source);
+    public abstract List<String> getSourcesFromPage(String page);
+    public int saveMemoryToFile(boolean resetMemory, String filePath) throws IOException {
+        String[] header = crawlProperties.getCSVHeader();
+        var result = fileService.writeCSV(header, this.memory, filePath);
+        if (resetMemory) {
+            this.memory.clear();
+        }
+        return result;
+    }
+}
