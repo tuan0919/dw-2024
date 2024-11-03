543845d6f34541bd0615dd54e41761f8d070a65a
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-11-04 05:00:04 +0700

    update một ít về enttiy

--- datawarehouse-crawl/src/main/java/com/nlu/app/entity/ProcessProperties.java
+++ datawarehouse-crawl/src/main/java/com/nlu/app/entity/ProcessProperties.java
@@ -1,10 +1,14 @@
 package com.nlu.app.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProcessProperties {
     String property_id;
     String name;
