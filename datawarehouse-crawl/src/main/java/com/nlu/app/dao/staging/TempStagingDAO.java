e6bf5d2a2f3b95cc76e82a37523a5e4c15983ea4
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-11-13 23:50:40 +0700

    add some related dbstaging methods

--- datawarehouse-crawl/src/main/java/com/nlu/app/dao/staging/TempStagingDAO.java
+++ datawarehouse-crawl/src/main/java/com/nlu/app/dao/staging/TempStagingDAO.java
@@ -3,12 +3,20 @@ package com.nlu.app.dao.staging;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.sqlobject.SqlObject;
 
+import java.time.LocalDate;
+
 public interface TempStagingDAO extends SqlObject {
-    default void insertToTempStaging() {
+    default void insertToTempStaging(LocalDate date) {
         Handle handle = this.getHandle();
-        handle.createCall("{call load_csv_to_temp_staging()}")
+        handle.createCall("{call load_csv_to_temp_staging(:date)}")
+                .bind("date", date)
                 .invoke();
         var procedure = handle.createQuery("SELECT @sql").mapTo(String.class).one();
+        var logId = handle.createQuery("SELECT @log_id").mapTo(int.class).one();
         handle.execute(procedure);
+        handle.createCall("{call update_log_status(:log_id, :new_status)}")
+                .bind("log_id", logId)
+                .bind("new_status", "L_SE")
+                .invoke();
     }
 }
