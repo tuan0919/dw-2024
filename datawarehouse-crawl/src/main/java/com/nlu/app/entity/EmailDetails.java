5514e9f513ec30b59f8b0caefe51e065dc93197b
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-12-08 18:40:59 +0700

    thêm chức năng gửi mail và các file cấu hình liên quan

---
+++ datawarehouse-crawl/src/main/java/com/nlu/app/entity/EmailDetails.java
@@ -0,0 +1,16 @@
+package com.nlu.app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDetails {
+    // Class data members
+    private String recipient;
+    private String msgBody;
+    private String subject;
+    private String attachment;
+}
